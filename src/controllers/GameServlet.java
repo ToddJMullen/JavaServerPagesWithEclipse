a305d50d793891af6f7662b479f9045926bc56c8
Todd Mullen <ToddJ.Mullen@gmail.com>
2016-12-04 15:03:05 -0500

    Add the GameServlet class to process incoming game values and determin round status.
    The bulk of the work in series two was the end of the last video (adding HTML line by line in the GameServlte controller -- so I didn't do most of it.

---
+++ src/controllers/GameServlet.java
@@ -0,0 +1,95 @@
+package controllers;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import model.GameNumber;
+
+/**
+ * Servlet implementation class GameServlet
+ */
+@WebServlet(description = "Processes the player's guess and creates a response to send back.", urlPatterns = { "/guess" })
+public class GameServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public GameServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see Servlet#init(ServletConfig)
+	 */
+	public void init(ServletConfig config) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//get incoming game values
+		GameNumber min 		= new GameNumber(Integer.parseInt(request.getParameter("min")));
+		GameNumber max 		= new GameNumber(Integer.parseInt(request.getParameter("max")));
+		GameNumber guess 	= new GameNumber(Integer.parseInt(request.getParameter("guess")));
+		GameNumber target 	= new GameNumber(Integer.parseInt(request.getParameter("target")));
+		GameNumber guesses	= new GameNumber(Integer.parseInt(request.getParameter("guesses")));
+		
+		//analyze the values
+		//formulate response
+		
+		String msg = "You guessed " + guess.getValue();
+		if (guess.getValue() == target.getValue()) {
+			msg += " correctly in " + guesses.getValue() + " guesses.";
+		} else {
+			msg += (guess.getValue() > target.getValue()) ? " and that's too high." : " and that's too low.";
+			guesses.increment();
+		}
+		
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		
+		//I'm not going to waste time copy/pasting every line of index.jsp into println() like at ~20m
+		//Turns out that the gist of this video / series is an exercise in the bad habit of generating
+		//view in the controller -- don't do it.
+		
+		out.println(msg);
+		
+//		response.getWriter().append("This was added by GameServlet::doPost()");
+//		doGet(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPut(HttpServletRequest, HttpServletResponse)
+	 */
+	protected void doPut(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see HttpServlet#doDelete(HttpServletRequest, HttpServletResponse)
+	 */
+	protected void doDelete(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
