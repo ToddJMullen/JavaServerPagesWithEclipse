8127b066ac59ed78c58cbf194b08b657532f70d2
Todd Mullen <ToddJ.Mullen@gmail.com>
2016-12-03 22:41:06 -0500

    Initial commit. Marked at the beginning of video 6: https://www.youtube.com/watch?v=Tjx76-Z1vIs&index=6&list=PLNUXdYCM8eNcRQKsib_XKKsy5kYN30Lh7

---
+++ README.md
@@ -0,0 +1,6 @@
+# Java / JSP Guessing Game Project
+
+Following along with Java / Servlet / JSP Project by Craig Piercy's Youtube Channel.
+Playlist: https://www.youtube.com/playlist?list=PLNUXdYCM8eNcRQKsib_XKKsy5kYN30Lh7
+
+
---
+++ src/model/GameNumber.java
@@ -0,0 +1,80 @@
+/**
+ * 
+ */
+package model;
+
+import java.util.Random;
+
+/**
+ * @author chaos7703
+ *
+ */
+public class GameNumber {
+	
+	private int value;
+
+	/**
+	 * 
+	 */
+	public GameNumber()
+	{
+	}
+
+
+	/**
+	 * @param value
+	 */
+	public GameNumber(int value)
+	{
+		if (value < 0) {
+			this.value = 0;
+		} else {
+			this.value = value;
+		}		
+	}
+
+
+
+	/**
+	 * @return the value
+	 */
+	public int getValue()
+	{
+		return value;
+	}
+
+	/**
+	 * @param value the value to set
+	 */
+	public void setValue(int value)
+	{
+		if (value < 0) {
+			this.value = 0;
+		} else {
+			this.value = value;
+		}		
+	}
+	
+	/**
+	 * Generates pseudo-random number between minimum & maximum
+	 * @param minimum The minimum acceptable value
+	 * @param maximum The maximum acceptable value
+	 */
+	public void setRandom(int minimum, int maximum)
+	{
+		Random random = new Random();
+		this.value = minimum + random.nextInt(maximum - minimum);
+	}
+	
+	
+	/**
+	 * Increments the value by one
+	 */
+	public void increment()
+	{
+		this.value++;
+	}
+
+	
+
+}
